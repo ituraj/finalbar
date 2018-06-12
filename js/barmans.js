0568cc1cba0e2db0f56d5710822910c0574cbe01
Wioletta Gluza <wiolagluza@gmail.com>
2018-06-12 16:41:31 +0200

    other approach of adding diamonds so we can actually edit them

--- js/barmans.js
+++ js/barmans.js
@@ -1,107 +1,109 @@
-document.addEventListener("DOMContentLoaded", loadSvg);
+let datas= JSON.parse(FooBar.getData());
 
-async function loadSvg() {
-    // Load SVG
-
-    let svgData = await fetch("diamond.svg");
-    let mySvg = await svgData.text();
-
-    document.querySelector("#diamond").innerHTML = mySvg;
-}
-// let datas= JSON.parse(FooBar.getData());
-
-// let bartendersLength = datas.bartenders.length;
+let bartendersLength = datas.bartenders.length;
 
 
-// function start(){
+function start(){
 
-//     let bartendersWrapper = document.createElement('div');
-//     bartendersWrapper.id = 'bartendersWrapper';
-//     document.body.appendChild(bartendersWrapper);
+    let bartendersWrapper = document.createElement('div');
+    bartendersWrapper.id = 'bartendersWrapper';
+    document.body.appendChild(bartendersWrapper);
    
 
 
 
 
-//     for(let i= 0; i<bartendersLength; i++){
-//         let bartendersContainer = document.createElement('div');
+    for(let i= 0; i<bartendersLength; i++){
+        let bartendersContainer = document.createElement('div');
 
-//         let newDiamond = document.createElement('img');
-//         let newSvg = document.createElement('img');
-//         let newName = document.createElement('p');
+        let newSvg = document.createElement('img');
+        let newName = document.createElement('p');
 
-//          let newStatus = document.createElement('p');
-//          let newStatusDetail = document.createElement('p');
-         
+         let newStatus = document.createElement('p');
+         let newStatusDetail = document.createElement('p');
 
+        loadSvg(i);
 
-//         newSvg.id = 'bartenderSvg'+i;
-//         newStatus.id = 'status'+i;
-//         newName.id = 'bartenderName'+i;
-//         newStatusDetail.id = 'statusDetail'+i;
-//         bartendersContainer.id = 'bartendersContainer';
-//         newDiamond.id = 'diamond' + i;
+        newSvg.id = 'bartenderSvg'+i;
+        newStatus.id = 'status'+i;
+        newName.id = 'bartenderName'+i;
+        newStatusDetail.id = 'statusDetail'+i;
+        bartendersContainer.id = 'bartendersContainer';
 
 
 
-//         newSvg.className = 'bartenderSvg';
-//         newStatus.className = 'status';
-//         newStatusDetail.className = 'statusDetail';
-//         newName.className = 'bartenderName';
-//         newDiamond.className = 'diamond';
+        newSvg.className = 'bartenderSvg';
+        newStatus.className = 'status';
+        newStatusDetail.className = 'statusDetail';
+        newName.className = 'bartenderName';
 
-//         newSvg.setAttribute("src", "images/" + datas.bartenders[i].name + ".svg");
-//         newDiamond.setAttribute("src", "images/diamond.svg");
-//         newStatusDetail.innerHTML = datas.bartenders[i].statusDetail;
+        newSvg.setAttribute("src", "images/" + datas.bartenders[i].name + ".svg");
+        newStatusDetail.innerHTML = datas.bartenders[i].statusDetail;
 
-//         bartendersWrapper.appendChild(bartendersContainer);
-//         bartendersContainer.appendChild(newDiamond);
-//         bartendersContainer.appendChild(newSvg);
-//         bartendersContainer.appendChild(newName);
-//         bartendersContainer.appendChild(newStatus);
-//         bartendersContainer.appendChild(newStatusDetail);
+        bartendersWrapper.appendChild(bartendersContainer);
+        bartendersContainer.appendChild(newSvg);
+        bartendersContainer.appendChild(newName);
+        bartendersContainer.appendChild(newStatus);
+        bartendersContainer.appendChild(newStatusDetail);
 
+        
+        async function loadSvg(i) {
 
+            let newDiv = document.createElement('div');
+            
+            // Load SVG
+        
+            let svgData = await fetch("images/diamond.svg");
+            let mySvg = await svgData.text();
+            console.log(i);
+            
+            
+            newDiv.id = 'diamond'+i;
+            newDiv.innerHTML = mySvg;
+
+            document.getElementById("diamonds").appendChild(newDiv);
+
+        }
        
         
 
-//     }
+    }
+}
+
+
+function updateBartenders(){
+
+         datas = FooBar.getData();
+         let data = JSON.parse(datas);
+         let currentStatus;
+
+    for(let i= 0; i<bartendersLength; i++){
+
+        currentStatus = data.bartenders[i].status;
+
+    
+        document.getElementById("status"+i).innerHTML = data.bartenders[i].status;
+        document.getElementById("bartenderName"+ i).innerHTML = data.bartenders[i].name;
+        document.getElementById("statusDetail"+ i).innerHTML = data.bartenders[i].statusDetail;
+
+    
+    }
+
+
+
+}
+
+// function setDiamondColor(diamond, color){
+//     diamond.
 // }
 
 
-// function updateBartenders(){
 
-//          datas = FooBar.getData();
-//          let data = JSON.parse(datas);
-//          let currentStatus;
-
-//     for(let i= 0; i<bartendersLength; i++){
-
-//         currentStatus = data.bartenders[i].status;
-
-    
-//         document.getElementById("status"+i).innerHTML = data.bartenders[i].status;
-//         document.getElementById("bartenderName"+ i).innerHTML = data.bartenders[i].name;
-//         document.getElementById("statusDetail"+ i).innerHTML = data.bartenders[i].statusDetail;
-
-    
-//     }
-
-
-
-// }
-
-// // function setDiamondColor(diamond, color){
-// //     diamond.
-// // }
-
-
-
-// window.setInterval(function(){
-//     updateBartenders();
+window.setInterval(function(){
+    updateBartenders();
     
  
     
-//    }, 3000);
+   }, 3000);
 
-//    start();
+   start();
