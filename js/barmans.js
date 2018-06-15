e118505662231a72a23d4b10fe7dfa858144c3cd
mazur <grzaneczka@gmail.com>
2018-06-15 04:47:55 +0200

    last comments

--- js/barmans.js
+++ js/barmans.js
@@ -39,50 +39,61 @@ function countPopularity(arr1, arr2){  ////creates array in which beernumber fro
 }
 
 
-function start(){
+function start(){   /// function that populates all of the areas on page load
+
+
+
+    storageState();             //calling all the different functions populating different "bar areas"
+    tapsState();
+    queueState();
+    loadKeg();
+    tapsOnMainBar();
+    createBartenders();
+    topBeersState();
+    setTimeout(function(){loadTapLabels();}, 2500);
+
+ 
+}
+
+
+
+
+
+function topBeersState(){  //populating blackboard with most popular beers
+
+    for(let i= 0; i<beersLength; i++){ 
+        let newTopBeer = document.createElement('p');
+        newTopBeer.id = 'topBeer'+i;
+        newTopBeer.className = 'topBeer'
+        newTopBeer.innerHTML = i+1 + '. <span id = "topBeerSpan'+i+'"></span>'  ;
+
+        if(i<5){
+        row1.appendChild(newTopBeer);
+        }
+        else{
+        row2.appendChild(newTopBeer);
+        }
+        beerNames.push(data.beertypes[i].name);
+
+}
+
+}
+
+
+
+
+function createBartenders(){   //main bar gets the bartenders
+
 
     let bartendersWrapper = document.createElement('div');
     bartendersWrapper.id = 'bartendersWrapper';
     document.body.appendChild(bartendersWrapper);
 
-    storageState();
-    tapsState();
-    queueState();
-    loadKeg();
-    setTimeout(function(){loadTapLabels();}, 2500);
-    tapsOnMainBar();
-
-
-
-    for(let i= 0; i<beersLength; i++){ //populating blackboard with most popular beers
-            let newTopBeer = document.createElement('p');
-            newTopBeer.id = 'topBeer'+i;
-            newTopBeer.className = 'topBeer'
-            newTopBeer.innerHTML = i+1 + '. <span id = "topBeerSpan'+i+'"></span>'  ;
-
-            if(i<5){
-            row1.appendChild(newTopBeer);
-            }
-            else{
-            row2.appendChild(newTopBeer);
-            }
-            beerNames.push(data.beertypes[i].name);
-
-    }
-  
-
-
-
-    
-   
-
-
-
 
     for(let i= 0; i<bartendersLength; i++){    //loop that populates bartenders with their details
         let bartendersContainer = document.createElement('div');
 
-        let newSvg = document.createElement('img');
+        let newSvg = document.createElement('img'); //svg in form of img because we dont need to manipulate any of the components
         let newName = document.createElement('p');
 
 
@@ -100,7 +111,7 @@ function start(){
         newSvg.className = 'bartenderSvg';
         newName.className = 'bartenderName';
 
-        newSvg.setAttribute("src", "images/" + data.bartenders[i].name + ".svg");
+        newSvg.setAttribute("src", "images/" + data.bartenders[i].name + ".svg");  //trick so it was easier to populate
 
         newName.innerHTML = data.bartenders[i].name;
 
@@ -109,18 +120,13 @@ function start(){
         bartendersContainer.appendChild(newName);
 
 
-      
-
-       
-        
-
     }
+
+
 }
 
 
-
-
-function updateDiamonds(){
+function updateDiamonds(){              //updating diamond colors depending on the bartender
 
     for(let i= 0; i<bartendersLength; i++){
         diamondColour(i);
@@ -130,18 +136,18 @@ function updateDiamonds(){
 }
 
 
-function diamondColour(i){
+function diamondColour(i){  ///based on state it shows different colors of diamond above bartenders head
 
-    if (data.bartenders[i].status=="WORKING") {
+    if (data.bartenders[i].status=="WORKING") {  //if he is busy its red
         
         $("#diamond"+i+">#diamond>#dcolour").css("fill", "#d85459")
         $("#diamond"+i+">#diamond>#inside").css("fill", "#d85459")
 
 
-    } else if (data.bartenders[i].status=="READY") {
+    } else if (data.bartenders[i].status=="READY") {  //if he is ready for work its green
         $("#diamond"+i+">#diamond>#dcolour").css("fill", "#59D854")
         $("#diamond"+i+">#diamond>#inside").css("fill", "#59D854")
-    } else {
+    } else {                                                            //else it is grey(it's for the future)
         $("#diamond"+i+">#diamond>#dcolour").css("fill", "#d3d3d3")
         $("#diamond"+i+">#diamond>#inside").css("fill", "#d3d3d3")
 
@@ -150,7 +156,7 @@ function diamondColour(i){
 }
 
 
-function queueState(){
+function queueState(){                                      //simply fetches the number of people(groups?) in the queue
     let queueDiv = document.getElementById("queueDiv");
     let newQueueNumber = document.createElement('p');
 
@@ -164,7 +170,7 @@ function queueState(){
 }
 
 
-function tapsState(){
+function tapsState(){                                       //function that populates the taps on the zoom bar
     let tempTapsArr=data.taps;
     let tap;
     let tapsDiv = document.getElementById("tapsDiv");
@@ -201,9 +207,9 @@ function tapsState(){
 
 }
 
-let breakCounterArr = Array.apply(null, Array(bartendersLength)).map(Number.prototype.valueOf,0);
+let breakCounterArr = Array.apply(null, Array(bartendersLength)).map(Number.prototype.valueOf,0); /// it populates the array with zeroes basing on bartenders length
 
-function checkIfBoredomBreak(){
+function checkIfBoredomBreak(){   ///if bartender is without any work for 5 seconds he informs that he is going to a break (console), we didn't manage to implement the actual break
 
     for(let i=0;i<bartendersLength; i++){
 
@@ -223,20 +229,16 @@ function checkIfBoredomBreak(){
 }
 }
 
-function checkIfTimeForBreak(){
-
-}
 
 
 
 
-
-function storageState(){
+function storageState(){            ///function that populates the storage section on the website basing on state of it on page load
     let tempKegsArr=data.storage;
     let keg;
     let amount;
     let kegsDiv = document.getElementById("kegsDiv");
-    for(let i=0; i<storageLength; i++){
+    for(let i=0; i<storageLength; i++){         ///loop that creates divs and adds names of beers in subsections
         keg= tempKegsArr[i].name;
         amount = tempKegsArr[i].amount;
 
@@ -252,7 +254,7 @@ function storageState(){
             newKeg.innerHTML = keg;
 
             newKegContainer.appendChild(newKeg);
-            for(let j = 0; j<amount; j++){
+            for(let j = 0; j<amount; j++){      ///this loop takes care of multiplying the keg images according to their amount in storage
                 let newSvg = document.createElement('img');
                 newSvg.id = 'barrelSvg'+j;
                 newSvg.className = 'barrelSvg';
@@ -267,15 +269,15 @@ function storageState(){
 
 }
 
-function updateStorage(){
+function updateStorage(){           //function that takes care of updating the storage
     let tempKegsArr=data.storage;
     let keg;
     let amount;
     let kegsDiv = document.getElementById("kegsDiv");
-    $('#kegsDiv div').find('img').remove()
+    $('#kegsDiv div').find('img').remove()          //removes all of the old images in the div
     for(let i=0; i<storageLength; i++){
-        amount = tempKegsArr[i].amount;
-        for(let j = 0; j<amount; j++){
+        amount = tempKegsArr[i].amount;             //checks for the current amount of kegs
+        for(let j = 0; j<amount; j++){              //and with the help of a loop inside of a loop populates subsections with right amount of kegs
             let newKegContainer = document.getElementById('kegContainerId'+ i);
             let newSvg = document.createElement('img');
             newSvg.id = 'barrelSvg'+j;
@@ -287,18 +289,18 @@ function updateStorage(){
     }
 }
 
-function updateTaps(){
+function updateTaps(){              //funciton that shows the current state of taps depending if they are being used right now
     let tempTapsArr=data.taps;
     let tap;
     let isInUse;
     let tapsDiv = document.getElementById("tapsDiv");
 
     for(let i=0; i<tempTapsArr.length; i++){
-        isInUse = tempTapsArr[i].inUse;
+        isInUse = tempTapsArr[i].inUse;                 //is it being used right now?
 
-        let handle = $("#svgContainerId"+i+">#Layer_1>#hand>#handle")
+        let handle = $("#svgContainerId"+i+">#Layer_1>#hand>#handle")       //difficult selection of deeply hidden svg layer through closest placed (unique obviously) id
 
-        if(isInUse==true){
+        if(isInUse==true){                              //if it is in use, animate the handle
         handle.css("fill", "grey");
         handle.css("transform", "rotate(90deg)");
         handle.css("transformOrigin", "55% 38% 0px");
@@ -315,7 +317,7 @@ function updateTaps(){
 
 
 
-async function loadKeg() {
+async function loadKeg() {                  //ASYNC function that allows us to place svg image in DOM with ability to access its children
 
     let tempTapsArr=data.taps;
 
@@ -338,7 +340,7 @@ async function loadKeg() {
 
 }
 
-function loadTapLabels(){
+function loadTapLabels(){               //function that makes labels of particular beers on currently used taps visible, while the others are hidden
     let tempTapsArr=data.taps;
     
 
@@ -353,7 +355,7 @@ function loadTapLabels(){
             beerLabelNr=j;
         }
     }
-    let x = $("#svgContainerId"+i+">#Layer_1>#keg>#lbl"+ beerLabelNr);
+    let x = $("#svgContainerId"+i+">#Layer_1>#keg>#lbl"+ beerLabelNr);      //three hours of struggling with this, didn't work because we tried to access it through async function
 
     
     x.css("visibility", "visible");
@@ -362,7 +364,7 @@ function loadTapLabels(){
 }
 
 
-function updateTopBeers(){
+function updateTopBeers(){      //puts two arrays into one so we can populate blackboard with amount of concrete beers bought in this session
 
   /////////DOWNLOADED FUNCTION FOR ARRAY MERGE I DONT UNDERSTAND THE WAY ITS DECLARED
         Array.prototype.zip = function (arr) {
@@ -375,11 +377,11 @@ function updateTopBeers(){
     let tempArraySorted = tempArray.sort(compareSecondColumn);
 
      for(let i = 0; i< beerNames.length; i++){
-     document.getElementById("topBeerSpan"+i).innerHTML = tempArraySorted[i][0] + " ("+tempArraySorted[i][1]+ ")";}
+     document.getElementById("topBeerSpan"+i).innerHTML = tempArraySorted[i][0] + " ("+tempArraySorted[i][1]+ ")";} //accessing two-dimensional array
 }
 
 
-async function loadSvg(i) {
+async function loadSvg(i) {     ///this function takes care of loading diamonds(current state indicators) for each bartender
 
     let newDiv = document.createElement('div');
    
@@ -398,7 +400,7 @@ async function loadSvg(i) {
 
 }
 
-function tapsOnMainBar(){
+function tapsOnMainBar(){                                       ////used to place svg taps on main bar, they indicate if the beer is being poured currently
     let mainTapsD = document.getElementById("mainTapsDiv");
     for(let i = 0; i<bartendersLength; i++){
         let newTapContainer = document.createElement('div');
@@ -409,7 +411,7 @@ mainTapsD.appendChild(newTapContainer);
 }
 
 
-function checkPopularity(){
+function checkPopularity(){             ///whenever the payment is being received, it pulls data about last order to beer popularity stats
 
     for(let i= 0; i<bartendersLength; i++){
        if(data.bartenders[i].statusDetail == "receivePayment"){
@@ -421,18 +423,18 @@ function checkPopularity(){
 }
 
 
-function USD(){
+function USD(){                             ///// graphically shows that bartender is receiving payment
     for(let i= 0; i<bartendersLength; i++){
         if(data.bartenders[i].statusDetail == "receivePayment"){
 
             document.querySelector("#dollar"+i).style.display="inline-block";
 
-            setTimeout(function(){ 
+            setTimeout(function(){                                      ///after the animation it hides so the animation could start again with next payment
             document.querySelector("#dollar"+i).style.display="none";
             }, 4000);
         }
     }}
-function animateBeerPouring(){
+function animateBeerPouring(){                      ////whenever bartender pours the beer the handle is animated
     for(let i= 0; i<bartendersLength; i++){
         let handle = $("#tapMainContainerId"+i+">#Layer_1>#hand>#handle")
 
@@ -452,7 +454,7 @@ function animateBeerPouring(){
     
 }
 
-function updateQueue(){
+function updateQueue(){         ////updates the number of orders in queue
     let queueNumber = document.getElementById("queueNumberId");
     queueLength = data.queue.length;
 
@@ -461,7 +463,7 @@ function updateQueue(){
 
 
 
-function updateData(){
+function updateData(){                  ///updates the data from which all of the necessary information is being gathered
     data = JSON.parse(FooBar.getData());
 }
 
@@ -496,13 +498,5 @@ window.setInterval(function(){  ///INTERVAL THAT UPDATES EVERY 1 SECS
 
 
 
-
-        window.setInterval(function(){  ///INTERVAL THAT UPDATES EVERY 10 SECS
-    
-            loadTapLabels();  ///// in case of slow loading
-           
-            }, 10000);
-
-
-   document.addEventListener("DOMContentLoaded", start());
+   document.addEventListener("DOMContentLoaded", start());          /// calls the start function when the dom content is loaded
     
